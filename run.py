d6043e9b678993eaa59554adfd1f73db27e666a1
Evgeniy Poturaev <gig177@yandex.ru>
2015-02-20 12:14:10 +0300

    flask entry point

---
+++ run.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+from flask import Flask, render_template, request, Response
+import os
+
+app = Flask(__name__, template_folder='view/templates',
+            static_folder='view')
+
+@app.route('/admin/')
+def index():
+    return 'index'
+    #return render_template('ab.html')
+    #return render_template('main.html')
+
+host = '0.0.0.0'
+host = '127.0.0.1'
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, host=host)
--- view/gulpfile.js
+++ view/gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'),
+var gulp = require('gulp');
 
 var installTask = require('gulp-task-install');
 
