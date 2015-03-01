718762e9b8f7d1332a57507b1c251b8afa450359
Evgeniy Poturaev <gig177@yandex.ru>
2015-03-01 18:52:04 +0300

    Установил bootstrap. Задачи less, lessc

--- view/gulpfile.js
+++ view/gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watchWrapper = require('gulp-watch-wrapper'),
+    mergeStream = require('merge-stream');
 
 var installTask = require('gulp-task-install');
+    lesscTask = require('gulp-task-lessc'),
 
 gulp.task('i', installTask);
+
+var lessMap = [
+    {'less/foo/bar.less' : ['less/foo/*.less']},
+    {'less/init.less'    : ['less/*.less', 'less/foo/*.less']}
+];
+gulp.task('less', function() {
+    watchWrapper(lessMap, lesscTask);
+});
+gulp.task('lessc', function() {
+    var merged = mergeStream();
+    lessMap.forEach(function(item) {
+        for (var target in item);
+        merged.add( lesscTask(target) );
+    });
+    return merged;
+});
