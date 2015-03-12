ea044953c780dfed0244ec8ba0f2d1ce1da9e096
Evgeniy Poturaev <gig177@yandex.ru>
2015-03-12 10:44:01 +0300

    gulp-task-swig installed

--- view/gulpfile.js
+++ view/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 
 var install = require('gulp-task-install');
     less = require('gulp-task-less'),
+    swig = require('gulp-task-swig');
 
 gulp.task('i', install);
 
@@ -13,4 +14,12 @@ gulp.task('less', function() {
 });
 gulp.task('lessc', function() {
     return less.compile(lessMap);
+});
+
+var tplMap = 'js/app/templates/**/*html';
+gulp.task('tpl', function() {
+    swig.watch(tplMap, { base : 'js' });
+});
+gulp.task('tplc', function() {
+    return swig.compile(tplMap, { base : 'js' });
 });
