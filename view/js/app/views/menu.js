97f6bdb1ab6f35dc9ee852b837d4ed8cd98ff69b
Evgeniy Poturaev <gig177@yandex.ru>
2015-03-15 14:57:21 +0300

    Чистка

--- view/js/app/views/menu.js
+++ view/js/app/views/menu.js
@@ -4,7 +4,6 @@ define(function(require) {
 
     var MenuCategory = require('app/collections/menu-category'),
         MenuCategoryView = require('app/views/menu-category'),
-        //menuTpl = require('tpl!app/templates/menu'),
         menuData = require('json!app/data/menu.json');
 
     var MenuView = Backbone.View.extend({
@@ -14,19 +13,10 @@ define(function(require) {
             this.render();
         },
         render: function() {
-            /*
-            this.categories.forEach(function(category) {
-                category.each(function(item) {
-                    this.renderMenuItem(item);
-                }, this);
-            }, this);
-            */
             menuData.forEach(function(category) {
                 this.renderCategory(category.category, category.list);
             }, this);
-            //this.$el.html( swig.run(menuTpl) );
             $('._sidebar').append(this.el);
-            //return this;
         },
         renderCategory: function(name, dataList) {
             var menuCategoryView = new MenuCategoryView({
@@ -35,14 +25,6 @@ define(function(require) {
             this.$el.append( menuCategoryView.render(name).el );
             //cl(menuCategoryView.render(name).el)
         },
-        /*
-        createCategories: function(categories) {
-            return categories.map(function(category) {
-                //cl('MenuCategory:', MenuCategory);
-                return new MenuCategory(category.list);
-            });
-        }
-        */
     });
 
     return MenuView;
