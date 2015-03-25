dc017d9e832d178eb5fcb90cba1eb52c04ea58ee
Evgeniy Poturaev <gig177@yandex.ru>
2015-03-25 11:32:34 +0300

    Рефакторинг. Открытие/закрытие элементов в действии

--- view/js/app/views/tree.js
+++ view/js/app/views/tree.js
@@ -31,7 +31,7 @@ define(function(require) {
     var NodeView = Backbone.View.extend({
         tagName: 'li',
         events: {
-            'click i': 'getChildren'
+            'click i': 'toggleChildren'
         },
         initialize: function() {
         },
@@ -39,44 +39,47 @@ define(function(require) {
             this.$el.html( swig.run(treeNodeTpl, this.model.attributes) );
             return this;
         },
-        setLoadingIcon: function() {
+        _setLoadingIcon: function() {
             this.$el.children('i').attr('class', 'glyphicon glyphicon-refresh fa-spin');
         },
-        setDefaultIcon: function() {
+        _setCloseIcon: function() {
             this.$el.children('i').attr('class', 'glyphicon glyphicon-circle-arrow-right');
         },
-        setOpenIcon: function() {
+        _setOpenIcon: function() {
             this.$el.children('i').attr('class', 'glyphicon glyphicon-circle-arrow-down');
         },
-        getChildren: function(e) {
+        toggleChildren: function(e) {
             e.stopPropagation();
-            //this.setLoadingIcon();
+            var m = this.model;
+            if (m.get('opened')) {
+                m.set('opened', false);
+                this._closeChildren();
+            } else {
+                m.set('opened', true);
+                this._openChildren();
+            }
+        },
+        _openChildren: function() {
+            cl('openChildren');
+            this._setOpenIcon();
 
-            /*
-            this.$el.find('i').velocity({ rotateZ : '45deg' }, { duration : 100, complete : function() {
-            }});
-            */
-            var childrenView = new TreeChildrenView( this.model.get('eid') );
-            var self = this;
-            childrenView.render().done(function(el) {
-                self.setOpenIcon();
-                /*
-                self.$el.find('i').velocity({ rotateZ : '45deg' }, { duration : 250, complete : function() {
-                    cl('animation complete');
-                }});
-                */
-                self.$el.append(el);
-                //self.$el.find('ul').velocity('transition.swoopIn');
-                //self.$el.find('ul').velocity('transition.swoopIn', { duration : 200 });
-                //self.$el.find('ul').velocity('transition.flipYIn', { duration : 200 });
-                //self.$el.find('ul').velocity('transition.shrinkIn', { duration : 200 });
-                //self.$el.find('ul').velocity('transition.bounceLeftIn', { duration : 200 });
-                //self.$el.find('ul').velocity('transition.slideDownIn', { duration : 200 });
-                self.$el.find('ul').velocity('transition.perspectiveLeftIn', { duration : 300 });
-                cl(
-                self.$el.find('ul')
-                )
-            });
+            var m = this.model, self = this;
+            if (!this._childrenView) {
+                this._childrenView = new TreeChildrenView( m.get('eid') );
+                this._childrenView.render().done(function(el) {
+                    self.$el.append(el);
+                    self._childrenView.open();
+                });
+                m.set('children', this._childrenView.collection);
+            } else {
+                this._childrenView.open();
+            }
+        },
+        _closeChildren: function() {
+            cl('closeChildren');
+            this._setCloseIcon();
+
+            this._childrenView.close();
         }
     });
 
@@ -102,6 +105,18 @@ define(function(require) {
         renderNode: function(m) {
             var nodeView = new NodeView({ model : m });
             this.$el.append( nodeView.render().el );
+        },
+        open: function() {
+            //this.$el.find('ul').velocity('transition.swoopIn');
+            //this.$el.find('ul').velocity('transition.swoopIn', { duration : 200 });
+            //this.$el.find('ul').velocity('transition.flipYIn', { duration : 200 });
+            //this.$el.find('ul').velocity('transition.shrinkIn', { duration : 200 });
+            //this.$el.find('ul').velocity('transition.bounceLeftIn', { duration : 200 });
+            //this.$el.find('ul').velocity('transition.slideDownIn', { duration : 200 });
+            this.$el.velocity('transition.perspectiveLeftIn', { duration : 300 });
+        },
+        close: function() {
+            this.$el.velocity('transition.perspectiveLeftOut', { duration : 300 });
         }
     });
 
