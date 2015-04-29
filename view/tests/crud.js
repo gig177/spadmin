eb10bb1bf8bc3b57f4d120c8a6496b85a179b715
Evgeniy Poturaev <gig177@yandex.ru>
2015-04-29 19:46:30 +0300

    save

--- view/tests/crud.js
+++ view/tests/crud.js
@@ -2,24 +2,40 @@ var assert = require('assert'),
     req = require('supertest')('http://localhost:5000'),
     Q = require('q');
 
-var deferred = Q.defer();
-describe('POST /api/catalog/', function() {
-    it('should create new item', function(done) {
-        var data = {
-            name: 'Barrack'
-        };
-        req.post('/api/catalog/')
-            .set('Content-Type', 'application/json')
-            .send(data)
-            .expect('Content-Type', /json/)
-            .expect(201)
-            .expect(function(res) {
-                assert(~~res.body.id);
-            })
-            .end(function(err, res) {
-                assert.ifError(err)
-                deferred.resolve(res.body)
-                done()
-            });
+var scrud = new SimpleCRUD('/api/catalog');
+scrud.create('should create new item');
+scrud.read('should read item');
+
+function SimpleCRUD(url) {
+    this._url = url;
+}
+SimpleCRUD.prototype.create = function(should) {
+    var deferred = Q.defer();
+    describe('POST /api/catalog/', function() {
+        it(should, function(done) {
+            var data = {
+                name: 'Barrack'
+            };
+            req.post('/api/catalog/')
+                .set('Content-Type', 'application/json')
+                .send(data)
+                .expect('Content-Type', /json/)
+                .expect(201)
+                .expect(function(res) {
+                    assert(~~res.body.id);
+                })
+                .end(function(err, res) {
+                    assert.ifError(err)
+                    deferred.resolve(res.body)
+                    done()
+                });
+        });
     });
-});
+    this._promise = deferred.promise;
+}
+SimpleCRUD.prototype.read = function() {
+}
+SimpleCRUD.prototype.update = function() {
+}
+SimpleCRUD.prototype.delete = function() {
+}
