3eb1b07209bdd1d123129bb515b5866930aa1da3
Evgeniy Poturaev <gig177@yandex.ru>
2015-05-08 12:29:30 +0300

    refactoring

--- view/tests/crud/simple.js
+++ view/tests/crud/simple.js
@@ -57,18 +57,7 @@ SimpleCRUD.prototype._unifiedRequest = function(method, status, item, should, va
     return deferred.promise;
 }
 SimpleCRUD.prototype.create = function(status, item, should, validators) {
-    var deferred = Q.defer();
-    var url = this._url;
-    describe('POST ' + url, function() {
-        it(should, function(done) {
-            req('post', url, status, item, validators).end(function(err, res) {
-                assert.ifError(err);
-                deferred.resolve(res.body.id);
-                done();
-            });
-        });
-    });
-    return deferred.promise;
+    return this._unifiedRequest('POST', status, item, should, validators);
 }
 SimpleCRUD.prototype.read = function(status, item, should, validators) {
     return this._unifiedRequest('GET', status, item, should, validators);
