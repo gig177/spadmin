a70fb11b68ab8d0762e325977813f6808a84a410
Evgeniy Poturaev <gig177@yandex.ru>
2015-05-11 15:55:19 +0300

    save

--- view/tests/tree-crud.js
+++ view/tests/tree-crud.js
@@ -1,20 +1,39 @@
 var cl = console.log;
-var tree = require('./crud/tree');
 var assert = require('assert');
+var req = require('supertest')('http://localhost:5000');
+
+var colors = require('mocha/lib/reporters/base').colors;
+colors['diff added'] = '30;42';
+colors['diff removed'] = '30;41';
 
 var page = {
     name: 'r\'o"o?t*/+' // root
 };
 
-var crud = new tree.TreeCRUD('/api/catalog');
-crud.create(201, page, 'should create a new item', CreateValidator);
-
-
-function CreateValidator() {}
-CreateValidator.prototype = new tree.BaseValidator();
-CreateValidator.prototype.name = function(value) {
-    assert.equal(value, 'root');
-}
-CreateValidator.prototype.title = function(value) {
-    assert.equal(value, 'root');
-}
+var resp = null;
+before(function(done) {
+    req.post(url)
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .send(page)
+        .expect(201)
+        .end(function(err, res) {
+            assert.ifError(err);
+            resp = res.body;
+            done();
+        });
+});
+var url = '/api/catalog';
+describe('POST ' + url, function() {
+    it('should create a new item', function() {
+        assert.equal(resp.id, 35)
+        /*
+        assert.strictEqual(resp, {
+            id: 34,
+            name: 'root',
+            title: 'root'
+        });
+        */
+    });
+});
+//it('should create a new item' 'should throw an error when an item is duplicated', function() {
