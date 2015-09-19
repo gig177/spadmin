401490af233f90bb5a7b5c49a980b0ffc7761fea
Evgeniy Poturaev <gig177@yandex.ru>
2015-09-19 12:47:05 +0300

    app reachable across the network

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 from app import app
 
+#host = '127.0.0.1'
 host = '0.0.0.0'
-host = '127.0.0.1'
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host=host)
